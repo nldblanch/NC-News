67ced06af2c29361ce3100fb8ca5df8b07361e1f
Nathan <nldblanch@gmail.com>
2024-07-17 11:20:59 +0100

    add topic filter to get articles endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -243,87 +243,131 @@ describe("/api/articles", () => {
           });
         });
     });
-    describe("?sort_by=", () => {
-      it("200: allows the user to sort articles by title, default alphabetical order", () => {
-        return request(app)
-          .get("/api/articles?sort_by=title")
+    describe("Queries ???", () => {
+      describe("?sort_by=", () => {
+        it("200: allows the user to sort articles by title, default alphabetical order", () => {
+          return request(app)
+            .get("/api/articles?sort_by=title")
+            .expect(200)
+            .then(({ body: { articles } }) => {
+              expect(articles).toBeSortedBy("title", {
+                descending: false,
+              });
+            });
+        });
+        it("200: allows the user to sort articles by any valid column (topic, default alphabetical)", () => {
+          return request(app)
+            .get("/api/articles?sort_by=topic")
+            .expect(200)
+            .then(({ body: { articles } }) => {
+              expect(articles).toBeSortedBy("topic", {
+                descending: false,
+              });
+            });
+        });
+        it("200: allows the user to sort articles by any valid column (author, default alphabetical)", () => {
+          return request(app)
+            .get("/api/articles?sort_by=author")
+            .expect(200)
+            .then(({ body: { articles } }) => {
+              expect(articles).toBeSortedBy("author", {
+                descending: false,
+              });
+            });
+        });
+        it("200: allows the user to sort articles by any valid column (votes, default descending)", () => {
+          return request(app)
+            .get("/api/articles?sort_by=votes")
+            .expect(200)
+            .then(({ body: { articles } }) => {
+              expect(articles).toBeSortedBy("votes", {
+                descending: true,
+              });
+            });
+        });
+        it("200: allows the user to sort articles by any valid column (created_at, default descending)", () => {
+          return request(app)
+            .get("/api/articles?sort_by=created_at")
+            .expect(200)
+            .then(({ body: { articles } }) => {
+              expect(articles).toBeSortedBy("created_at", {
+                descending: true,
+              });
+            });
+        });
+        it("405: only allows greenlisted sortby queries", () => {
+          return request(app)
+            .get("/api/articles?sort_by=bananas")
+            .expect(405)
+            .then(({ body: { message } }) => {
+              expect(message).toBe("405 - Method Not Allowed");
+            });
+        });
+      });
+      describe("?order=", () => {
+        it("200: allows user to choose whether the order is ascending or descending", () => {
+          return request(app)
+            .get("/api/articles?sort_by=votes&&order=asc")
+            .expect(200)
+            .then(({ body: { articles } }) => {
+              expect(articles).toBeSortedBy("votes", {
+                descending: false,
+              });
+            });
+        });
+        it("200: defaults to descending (alphabetical ascending) when invalid order given", () => {
+          return request(app)
+            .get("/api/articles?sort_by=author&&order=anything_I_want")
+            .expect(200)
+            .then(({ body: { articles } }) => {
+              expect(articles).toBeSortedBy("author", {
+                descending: false,
+              });
+            });
+        });
+      })
+      describe("?topic=", () => {
+        it("200: allows user to filter articles by topic", () => {
+          const input_topic = "mitch"
+          return request(app)
+          .get(`/api/articles?topic=${input_topic}`)
           .expect(200)
           .then(({ body: { articles } }) => {
-            expect(articles).toBeSortedBy("title", {
-              descending: false,
-            });
+            articles.forEach((article) => {
+              expect(article.topic).toBe(input_topic)
+            })
           });
-      });
-      it("200: allows the user to sort articles by any valid column (topic, default alphabetical)", () => {
-        return request(app)
-          .get("/api/articles?sort_by=topic")
+        })
+        it("200: returns with empty array when search topic exists but yields no results", () => {
+          const input_topic = "paper"
+          return request(app)
+          .get(`/api/articles?topic=${input_topic}`)
           .expect(200)
           .then(({ body: { articles } }) => {
-            expect(articles).toBeSortedBy("topic", {
-              descending: false,
-            });
+            expect(articles).toEqual([])
           });
-      });
-      it("200: allows the user to sort articles by any valid column (author, default alphabetical)", () => {
-        return request(app)
-          .get("/api/articles?sort_by=author")
-          .expect(200)
-          .then(({ body: { articles } }) => {
-            expect(articles).toBeSortedBy("author", {
-              descending: false,
-            });
-          });
-      });
-      it("200: allows the user to sort articles by any valid column (votes, default descending)", () => {
-        return request(app)
-          .get("/api/articles?sort_by=votes")
-          .expect(200)
-          .then(({ body: { articles } }) => {
-            expect(articles).toBeSortedBy("votes", {
-              descending: true,
-            });
-          });
-      });
-      it("200: allows the user to sort articles by any valid column (created_at, default descending)", () => {
-        return request(app)
-          .get("/api/articles?sort_by=created_at")
-          .expect(200)
-          .then(({ body: { articles } }) => {
-            expect(articles).toBeSortedBy("created_at", {
-              descending: true,
-            });
-          });
-      });
-      it("405: only allows greenlisted sortby queries", () => {
-        return request(app)
-          .get("/api/articles?sort_by=bananas")
-          .expect(405)
+        })
+        it("404: returns with error when search topic does not exist", () => {
+          const input_topic = "nathan"
+          return request(app)
+          .get(`/api/articles?topic=${input_topic}`)
+          .expect(404)
           .then(({ body: { message } }) => {
-            expect(message).toBe("405 - Method Not Allowed");
+            expect(message).toEqual("404 - Not Found")
           });
-      });
-    });
-    describe("?order=", () => {
-      it("200: allows user to choose whether the order is ascending or descending", () => {
+        })
+      })
+      it("200: does not attempt any sort, resulting in default, when invalid sort key given", () => {
+        const wrongKey = 'bananas'
         return request(app)
-          .get("/api/articles?sort_by=votes&&order=asc")
-          .expect(200)
-          .then(({ body: { articles } }) => {
-            expect(articles).toBeSortedBy("votes", {
-              descending: false,
-            });
+            .get(`/api/articles?${wrongKey}=votes`)
+            .expect(200)
+            .then(({ body: { articles } }) => {
+              expect(articles).toBeSortedBy("created_at", {
+            descending: true,
           });
-      });
-      it("200: defaults to descending (alphabetical ascending) when invalid order given", () => {
-        return request(app)
-          .get("/api/articles?sort_by=author&&order=anything_I_want")
-          .expect(200)
-          .then(({ body: { articles } }) => {
-            expect(articles).toBeSortedBy("author", {
-              descending: false,
-            });
-          });
-      });
+        });
+      })
     })
   });
   
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -32,8 +32,8 @@ exports.getArticleById = (request, response, next) => {
 };
 
 exports.getArticles = (request, response, next) => {
-  const {sort_by, order} = request.query
-  fetchArticles(sort_by, order)
+  const {sort_by, order, topic} = request.query
+  fetchArticles(sort_by, order, topic)
   .then((articles) => {
     response.status(200).send({ articles });
   })
