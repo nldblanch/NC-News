7757cc0d036105661ad27c4cfe9ecbdfcad0abbc
Nathan <nldblanch@gmail.com>
2024-07-18 17:01:29 +0100

    added pagination

--- routers/articles-router.js
+++ routers/articles-router.js
@@ -14,7 +14,10 @@ articlesRouter
 .get(getArticles)
 .post(postArticle)
 
-articlesRouter.route("/:article_id").get(getArticleById).patch(patchArticle);
+articlesRouter
+.route("/:article_id")
+.get(getArticleById)
+.patch(patchArticle);
 
 articlesRouter
   .route("/:article_id/comments")
