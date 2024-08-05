054d424bdef253953247b06265f5a04b54c00da1
Nathan <nldblanch@gmail.com>
2024-08-05 09:48:31 +0100

    allow for cross origin resource sharing

--- api/app.js
+++ api/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const errorFunctions = require("./error-handling");
+const cors = require("cors")
 const app = express();
+app.use(cors())
 const apiRouter = require("./routers/api-router")
 app.use(express.json());
 app.use("/api", apiRouter)
+const errorFunctions = require("./error-handling");
 
 app.all("*", errorFunctions.invalidEndpoint);
 
